ee5842a37c63a9f15bccd4b07fbe8cb0cf9abd0c
kturk0 <46371512+kturk0@users.noreply.github.com>
2022-04-24 21:00:37 +0200

    fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: 'http://192.168.1.13:5000',
+ baseURL: '##################',
 });
 
 export default api;
-export const server = 'http://192.168.1.13:5000';
+export const server = '##########';
